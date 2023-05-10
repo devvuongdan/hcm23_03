b0860fe8477832c287fec78b1c5be62116f03fa7
voxuannhat <nhatvobh22230@gmail.com>
2023-05-10 17:44:51 +0700

    clone code

--- lib/pratices_dart_basic/ex1_arithmetic_operators--1.dart
+++ lib/pratices_dart_basic/ex1_arithmetic_operators--1.dart
@@ -5,6 +5,15 @@ out a message that tells how many years they have to be 100 years old.
 Diff: 1
 */
 
+import 'dart:io';
 
+void main() {
+  
+  print('Name: ');
+  String? name = stdin.readLineSync();
+  print('Age: ');
+  int age = int.parse(stdin.readLineSync()!); // khong tra ve null
+      int years = 100 - age;
+  print('$name, you have $years years left to be 100 years old');
 
-void main() {}
+}

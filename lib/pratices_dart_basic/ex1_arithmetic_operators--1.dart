d32abef26ca665b1036505ac8c645f215983b58b
VuongDan <dev.vuongdan@gmail.com>
2023-05-10 13:16:33 +0700

    Revert "docs: Update pratice ex for dart basic"
    This reverts commit 81aae8f302b001060efa8b106b69b44c99c833b3.

--- lib/pratices_dart_basic/ex1_arithmetic_operators--1.dart
+++
@@ -1,8 +0,0 @@
-/*
-Create a program that asks the user to enter their name and their age. Print 
-out a message that tells how many years they have to be 100 years old.
-
-Diff: 1
-*/
-
-void main() {}

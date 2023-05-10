2f4bcbbe32a96c80ca16d5b43d92e022ee2a46b5
hangnguyengiaminh <minhgiaha@gmail.com>
2023-05-10 10:29:18 +0700

    code run

--- lib/pratices_dart_basic/ex1_arithmetic_operators--1.dart
+++ lib/pratices_dart_basic/ex1_arithmetic_operators--1.dart
@@ -2,7 +2,22 @@
 Create a program that asks the user to enter their name and their age. Print 
 out a message that tells how many years they have to be 100 years old.
 
-Diff: 1
-*/
 
-void main() {}
+*/
+import 'dart:io';
+	
+
+	void main() {    
+	    stdout.write("What's your name? ");
+	    String name = stdin.readLineSync()!; //Null Safety
+	
+
+	    print("Hi, $name! What is your age?");
+	    int age = int.parse(stdin.readLineSync()!); //Null Safety
+	
+
+	    int yearsToHunderd = 100 - age;
+	    print("$name, You have $yearsToHunderd years to be 100");
+	}
+
+

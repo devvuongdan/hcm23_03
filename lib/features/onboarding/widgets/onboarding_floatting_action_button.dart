a49acc9e439c78137be9b368a6600e593b48eeb9
VuongDan <dev.vuongdan@gmail.com>
2023-05-15 21:27:46 +0700

    feat: add onboarding_floating_action_button

--- lib/features/onboarding/widgets/onboarding_floatting_action_button.dart
+++ lib/features/onboarding/widgets/onboarding_floatting_action_button.dart
@@ -1,82 +1,118 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/material.dart';
 
-class YinOnboardFloattingActionButton extends StatefulWidget {
-  final double startValue;
-  final double endValue;
-  const YinOnboardFloattingActionButton({
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class YinFloatingActionButtonArgs {
+  final double size;
+  final Color scaffoldBackgroundColor;
+  final int stepCount;
+  final int currentStep;
+
+  final void Function()? onTap;
+  YinFloatingActionButtonArgs({
+    required this.size,
+    required this.scaffoldBackgroundColor,
+    this.stepCount = 2,
+    required this.currentStep,
+    this.onTap,
+  }) : assert(currentStep <= stepCount);
+}
+
+class YinFloatingActionButton extends StatefulWidget {
+  final YinFloatingActionButtonArgs args;
+  const YinFloatingActionButton({
     Key? key,
-    required this.startValue,
-    required this.endValue,
+    required this.args,
   }) : super(key: key);
 
   @override
-  State<YinOnboardFloattingActionButton> createState() =>
-      _YinOnboardFloattingActionButtonState();
+  State<YinFloatingActionButton> createState() =>
+      _YinFloatingActionButtonState();
 }
 
-class _YinOnboardFloattingActionButtonState
-    extends State<YinOnboardFloattingActionButton>
+class _YinFloatingActionButtonState extends State<YinFloatingActionButton>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  bool _isButtonPressed = false;
+  late AnimationController _animationController;
+  double _progressValue = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
+    _animationController = AnimationController(
       vsync: this,
+      duration: const Duration(milliseconds: 500),
     );
-    _animation = Tween<double>(begin: 0.0, end: 0.5).animate(CurvedAnimation(
-      parent: _controller,
-      curve: const Interval(0.0, 0.5, curve: Curves.linear),
-    ));
 
-    _controller.addListener(() {
-      setState(() {}); // Khi giá trị hoạt hình thay đổi, cập nhật lại giao diện
+    _animationController.addListener(() {
+      setState(() {
+        _progressValue = _animationController.value;
+      });
     });
+    runProgress();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _animationController.dispose();
+
     super.dispose();
   }
 
-  void _startAnimation() {
-    _isButtonPressed = true;
-    _controller.reset();
-    _controller.forward().whenComplete(() {
-      if (_isButtonPressed) {
-        _animation =
-            Tween<double>(begin: 0.5, end: 1.0).animate(CurvedAnimation(
-          parent: _controller,
-          curve: const Interval(0.5, 1.0, curve: Curves.linear),
-        ));
-        _controller.reset();
-        _controller.forward();
-      }
-    });
+  void runProgress() {
+    if (_progressValue == 1) {
+      setState(() {
+        _progressValue = 0;
+      });
+    }
+    _animationController.animateTo(_progressValue + 1 / widget.args.stepCount,
+        duration: const Duration(milliseconds: 500));
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: _startAnimation,
+      onTap: () {
+        widget.args.onTap?.call();
+        runProgress();
+      },
       child: Container(
-        height: 58,
-        width: 58,
+        height: widget.args.size,
+        width: widget.args.size,
         decoration: const BoxDecoration(
           shape: BoxShape.circle,
         ),
         child: Stack(
           children: [
-            CircularProgressIndicator(
-              value: _animation.value,
-              color: const Color(0xFFFFFFFF),
-            )
+            SizedBox(
+              height: widget.args.size,
+              width: widget.args.size,
+              child: CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 3,
+                value: _progressValue,
+                backgroundColor: Colors.white.withOpacity(0.3),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: SvgPicture.asset(
+                    "assets/icons/angle_right_solid.svg",
+                    height: 20,
+                    width: 16,
+                    color: widget.args.scaffoldBackgroundColor,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),

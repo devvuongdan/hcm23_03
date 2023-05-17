8b88faed94c425dfb91d294edd8cd3badc6ccd9e
nguyentrinha3qt <nguyentrinha3qt@gmail.com>
2023-05-17 16:19:32 +0700

    btap

--- lib/features/onboarding/pages/onboarding_page.dart
+++ lib/features/onboarding/pages/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hcm23_03/features/onboarding/widgets/onboarding_floatting_action_button.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -20,13 +21,55 @@ class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
+      backgroundColor: Color.fromARGB(255, 104, 193, 238),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.only(left: 23, right: 25, top: 80),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Always there: more than 1000 cars in Tbilisi",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w700,
+                  height: 32 / 24,
+                  fontSize: 32,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w400,
+                  height: 24 / 19,
+                  fontSize: 19,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 8, top: 33),
+                child: Image.asset(
+                  "assets/images/onboarding_img_2.png",
+                  width: 300,
+                  height: 300,
+                ),
+              ),
+            ],
+          ),
+=======
       backgroundColor: const Color(0xFFF4C27F),
-      floatingActionButton: Container(
-        height: 58,
-        width: 58,
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.red,
+      floatingActionButton: YinFloatingActionButton(
+        args: YinFloatingActionButtonArgs(
+          size: 58,
+          scaffoldBackgroundColor: const Color(0xFFF4C27F),
+          currentStep: 0,
+          stepCount: 2,
+>>>>>>> Stashed changes
         ),
       ),
     );

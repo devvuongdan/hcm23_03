8f278fa6957eaa75397c0fddc09b5db3acdc4ba8
oboioz <lebatan8@gmail.com>
2023-05-19 21:19:03 +0700

    Update code

--- lib/features/onboarding/pages/onboarding_page.dart
+++ lib/features/onboarding/pages/onboarding_page.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-=======
 import 'package:hcm23_03/features/home/pages/todo_list_page.dart';
->>>>>>> origin/dev-v1
 import 'package:hcm23_03/features/onboarding/widgets/onboarding_floatting_action_button.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -25,17 +20,6 @@ class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-        backgroundColor: const Color.fromARGB(255, 170, 158, 236),
-        floatingActionButton: YinFloatingActionButton(
-          args: YinFloatingActionButtonArgs(
-            size: 60.0,
-            scaffoldBackgroundColor: const Color.fromARGB(255, 170, 158, 236),
-            stepCount: 2,
-            currentStep: 0,
-            onTap: continues,
-          ),
-=======
       floatingActionButton: YinFloatingActionButton(
         args: YinFloatingActionButtonArgs(
           size: 58,
@@ -179,52 +163,52 @@ class _OnboardingPageState extends State<OnboardingPage> {
               child: Image.asset(imageUrl),
             ),
           ],
->>>>>>> origin/dev-v1
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              Text('A Community Was Build Just For You',
-                  //   style: TextStyle(
-                  //     fontSize: 24,
-                  //     fontWeight: FontWeight.w700,
-                  //     height: 1.5,
-                  //     color: Colors.white,
+        // body: Padding(
+        //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+        //   child: Column(
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       const SizedBox(
+        //         height: 100,
+        //       ),
+        //       Text('A Community Was Build Just For You',
+        //           //   style: TextStyle(
+        //           //     fontSize: 24,
+        //           //     fontWeight: FontWeight.w700,
+        //           //     height: 1.5,
+        //           //     color: Colors.white,
 
-                  // ),
-                  style: GoogleFonts.poppins(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
-                    height: 1.2,
-                    color: Colors.white,
-                  )),
-              Text(
-                'A community was build for you to share your thought, your concerns and improve yourself with other people',
-                //   style: TextStyle(
-                //     fontSize: 18,
-                //     fontWeight: FontWeight.w400,
-                //     height: 1.5,
-                //     color: Colors.white,
-                // ),
-                style: GoogleFonts.lora(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                  height: 1.5,
-                  color: Colors.white,
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: SvgPicture.asset("assets/images/onboarding_img_1.svg"),
-                ),
-              ),
-            ],
-          ),
+        //           // ),
+        //           style: GoogleFonts.poppins(
+        //             fontSize: 24,
+        //             fontWeight: FontWeight.w700,
+        //             height: 1.2,
+        //             color: Colors.white,
+        //           )),
+        //       Text(
+        //         'A community was build for you to share your thought, your concerns and improve yourself with other people',
+        //         //   style: TextStyle(
+        //         //     fontSize: 18,
+        //         //     fontWeight: FontWeight.w400,
+        //         //     height: 1.5,
+        //         //     color: Colors.white,
+        //         // ),
+        //         style: GoogleFonts.lora(
+        //           fontSize: 18,
+        //           fontWeight: FontWeight.w400,
+        //           height: 1.5,
+        //           color: Colors.white,
+        //         ),
+        //       ),
+        //       Expanded(
+        //         child: Center(
+        //           child: SvgPicture.asset("assets/images/onboarding_img_1.svg"),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
         ));
   }
 }

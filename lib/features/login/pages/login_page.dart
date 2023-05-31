e0d7bb0ec961c33bf6cc77d189fee49f968a1940
phanminhhoang <minhhoang4019@gmail.com>
2023-05-31 20:14:57 +0700

    Rechange_Password

--- lib/features/login/pages/login_page.dart
+++ lib/features/login/pages/login_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  final List<String> _address = ["HN", "HCM"];
+  late String _addressValue;
+
+  bool _isRemember = false;
+  Gender _gender = Gender.male;
+  
+  @override
+  void initState() {
+    super.initState();
+    _addressValue = _address.first;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +38,160 @@ class LoginPage extends StatelessWidget {
         title: const Text("LoginPage"),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pushNamed("/HomePage");
-          },
-          child: const Text("Go to Home"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _passwordController,
+                  autofocus: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "This field can not be Empty";
+                    }
+                    if (value.length < 6) {
+                      return "Password is too weak";
+                    }
+                    return null;
+                  },
+                  obscureText: true,               
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your confirm password",
+                    prefixIcon: Icon(Icons.abc),
+                    suffixIcon: Icon(Icons.delete),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  autofocus: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "This field can not be Empty";
+                    }
+                    if (value.length < 6) {
+                      return "Password is too weak";
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your password",
+                    prefixIcon: Icon(Icons.remove_red_eye),
+                  ),
+                ),
+
+
+                CheckboxListTile(
+                  value: _isRemember,
+                  title: const Text(
+                    "Remember account",
+                  ),
+                  onChanged: ((value) {
+                    setState(() {
+                      _isRemember = value == true;
+                    });
+                  }),
+                ),
+
+
+
+                RadioListTile(
+                  title: const Text("Male"),
+                  value: Gender.male,
+                  groupValue: _gender,
+                  onChanged: (Gender? value) {
+                    setState(() {
+                      _gender = value!;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+
+                RadioListTile(
+                  title: const Text("Female"),
+                  value: Gender.female,
+                  groupValue: _gender,
+                  onChanged: (Gender? value) {
+                    setState(() {
+                      _gender = value!;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+
+                RadioListTile(
+                  title: const Text("Undefine"),
+                  value: Gender.undefine,
+                  groupValue: _gender,
+                  onChanged: (Gender? value) {
+                    setState(() {
+                      _gender = value!;
+                    });
+                  },
+                ),
+
+
+
+                DropdownButton<String> (
+                  value: _addressValue,
+                  items: _address.map((e) => DropdownMenuItem<String> (
+                    value: e,
+                    child: Row(
+                      children: [
+                        Text(e),
+                      ],
+                    ))).toList(),
+                  onChanged: ((value) {
+                    setState(() {
+                      _addressValue = value ?? "";
+                    });
+                  }),
+                ),
+                
+
+
+                const SizedBox(
+                  height: 40,
+                ),
+
+
+
+                ElevatedButton(
+                  onPressed: () {
+                    // final bool isValid = _formKey.currentState?.validate() == true;
+                    // ScaffoldMessenger.of(context).showSnackBar(
+                    //   SnackBar(content: Text("The validator is $isValid")));
+                    final String password = _passwordController.text;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("The password is $password"),
+                      ),
+                    );
+                  },
+                  child: const Text("Submit"),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+enum Gender {male, female, undefine}
---
+++ lib/features/login/pages/rechange_password.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class RechangePasswordPage extends StatefulWidget {
+  const RechangePasswordPage({super.key});
+  @override
+  State<RechangePasswordPage> createState() => _RechangePasswordPageState();
+}
+
+
+class _RechangePasswordPageState extends State<RechangePasswordPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _EmailController = TextEditingController();
+  final TextEditingController _AccountController = TextEditingController();
+
+   
+
+  @override
+  void dispose() {
+    super.dispose();
+    _EmailController.dispose();
+    _AccountController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rechange-PasswordPage"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _EmailController,
+                  autofocus: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "This field can not be Empty";
+                    }
+                    if (value.length < 6) {
+                      return "The Email/Phone number is incorrect.";
+                    }
+                    return null;
+                  },
+                  obscureText: true,               
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Email/Phone number",
+                    prefixIcon: Icon(Icons.mail),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                TextFormField(
+                  controller: _AccountController,
+                  autofocus: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "This field can not be Empty";
+                    }
+                    if (value.length < 6) {
+                      return "The Account name is incorrect.";
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Account name",
+                    prefixIcon: Icon(Icons.person_outline),
+                  ),
+                ),
+
+
+
+                const SizedBox(
+                  height: 40,
+                ),
+
+
+
+                ElevatedButton(
+                  onPressed: () {
+                    final String password = _EmailController.text;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("The Account is $password"),
+                      ),
+                    );
+                  },
+                  child: const Text("Next"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

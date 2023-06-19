c27afc66a757b70809fe68cce93f7951ecacb4c9
VuongDan <dev.vuongdan@gmail.com>
2023-06-19 22:01:23 +0700

    feat: Tasks repository

--- lib/features/tasks/entities/task_model.dart
+++ lib/features/tasks/entities/task_model.dart
@@ -25,12 +25,12 @@ class Task extends DBModel {
   @override
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
-      uid: map['uid'] as String,
-      userId: map['userId'] as String,
-      title: map['title'] as String,
-      description: map['description'] as String,
-      starttime: map['starttime'] as String,
-      duetime: map['duetime'] as String,
+      uid: map['uid'].toString(),
+      userId: map['userId'].toString(),
+      title: map['title'].toString(),
+      description: map['description'].toString(),
+      starttime: map['starttime'].toString(),
+      duetime: map['duetime'].toString(),
       teamMembers: List<TeamMember>.from(
         (map['teamMembers'] as List? ?? []).map<TeamMember>(
           (x) => TeamMember.fromMap(x as Map<String, dynamic>),
@@ -56,6 +56,11 @@ class Task extends DBModel {
       'teamMembers': teamMembers.map((x) => x.toMap()).toList(),
       'stages': stages.map((x) => x.toMap()).toList(),
     };
+  }
+
+  @override
+  String toString() {
+    return 'Task(userId: $userId, title: $title, description: $description, starttime: $starttime, duetime: $duetime, teamMembers: $teamMembers, stages: $stages)';
   }
 }
 
@@ -84,10 +89,10 @@ class TaskStage extends DBModel {
 
   factory TaskStage.fromMap(Map<String, dynamic> map) {
     return TaskStage(
-      uid: map['uid'] as String,
-      taskUid: map['taskUid'] as String,
+      uid: map['uid'].toString(),
+      taskUid: map['taskUid'].toString(),
       isDone: map['isDone'] == "true",
-      stageName: map['stageName'] as String,
+      stageName: map['stageName'].toString(),
     );
   }
 }
@@ -126,7 +131,7 @@ class TeamMember extends DBModel {
     return TeamMember(
       taskUid: map['taskUid'],
       uid: map['uid'],
-      avatarUrl: map['avatarUrl'] as String,
+      avatarUrl: map['avatarUrl'].toString(),
     );
   }
 }
